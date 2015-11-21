76a16ad7f1c4077f358ac98491754ca89ee51b31
Joshua Cox <joshua.n.cox@gmail.com>
2015-11-20 18:55:25 -0800

    refactored with underscore

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -22,29 +23,26 @@ app.get('/todos', function(req, res) {
 // GET /todos/:id
 app.get('/todos/:id', function(req, res) {
 	var id = parseInt(req.params.id, 10);
-	var matchedTodo;
-
-	todos.forEach(function(todo) {
-		if (todo.id === id) {matchedTodo = todo;}
-	});
-
+	var matchedTodo = _.findWhere(todos, {id: id});
+	
 	if (matchedTodo) {
 		res.json(matchedTodo);
 	} else {
 		res.status(404).send();
 	}
-
-	//res.send('Asking for todo with id of ' + req.params.id);
 })
 
 // POST /todos
 app.post('/todos', function( req, res ) {
 	var body = req.body;
-	console.log("desciption: " + req.body.description);
+
+	if (!_.isString(body.description) || body.description.trim().length == 0) {
+		return res.status(400).send();
+	}
 
 	var newTodo = {
 		id: nextTodoId++,
-		description: req.body.description,
+		description: req.body.description.trim(),
 		done: false
 	};
 
