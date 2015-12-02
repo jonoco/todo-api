da3ff8e2e0c0e0900066d80d723097a1e4e7a7e8
Joshua Cox <joshua.n.cox@gmail.com>
2015-12-01 20:27:07 -0800

    added auth middleware and findByToken class method

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var bodyParser = require('body-parser');
 var _ = require('underscore');
 var db = require('./db.js');
 var bcrypt = require('bcrypt');
+var middleware = require('./middleware.js')(db);
 
 var app = express();
 var PORT = process.env.PORT || 3000;
@@ -17,7 +18,7 @@ app.get('/', function( req, res ) {
 });
 
 // GET /todos
-app.get('/todos', function( req, res ) {
+app.get('/todos', middleware.requireAuthentication, function( req, res ) {
 	var query = req.query;
 	var where = {};
 
@@ -44,7 +45,7 @@ app.get('/todos', function( req, res ) {
 })
 
 // GET /todos/:id
-app.get('/todos/:id', function( req, res ) {
+app.get('/todos/:id', middleware.requireAuthentication, function( req, res ) {
 	var id = parseInt(req.params.id, 10);
 	
 	db.todo.findById(id).then(function(todo) {
@@ -56,7 +57,7 @@ app.get('/todos/:id', function( req, res ) {
 });
 
 // POST /todos
-app.post('/todos', function( req, res ) {
+app.post('/todos', middleware.requireAuthentication, function( req, res ) {
 	// Only pass through the description value
 	var body = _.pick(req.body, 'description');
 
@@ -69,7 +70,7 @@ app.post('/todos', function( req, res ) {
 });
 
 // DELETE /todos/:id
-app.delete('/todos/:id', function( req, res ) {
+app.delete('/todos/:id', middleware.requireAuthentication, function( req, res ) {
 	var id = parseInt(req.params.id, 10);
 	
 	db.todo.destroy({
@@ -88,7 +89,7 @@ app.delete('/todos/:id', function( req, res ) {
 });
 
 // PUT /todos/:id
-app.put('/todos/:id', function( req, res ) {
+app.put('/todos/:id', middleware.requireAuthentication, function( req, res ) {
 	var id = parseInt(req.params.id, 10);
 	var updatedTodo = {};
 
